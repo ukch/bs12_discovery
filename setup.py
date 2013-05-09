41766e47244a6cd26be65abe5e8f8033e7458fee
Joel Cross <joel@kazbak.co.uk>
2013-05-09 22:43:27 +0100

    Empty pyramid template

--- setup.py
+++ setup.py
@@ -1,15 +1,42 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-setup(name="bs12_discovery",
-      author="Joel Cross",
-      url="http://github.com/ukch/bs12_discovery",
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+# FIXME README will have ugly markdown formatting
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'pyramid_debugtoolbar',
+    'waitress',
+    ]
+
+setup(name='bs12_discovery',
+      author='Joel Cross',
+      author_email='joel@kazbak.co.uk',
+      url='http://github.com/ukch/bs12_discovery',
       version='0.1',
-      packages=['bs12_discovery'],
-      package_dir={'': 'src'},
-      package_data={},
-      zip_safe=True,
-)
+      description='bs12_discovery',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      keywords='web pyramid pylons',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      tests_require=requires,
+      test_suite="bs12_discovery",
+      entry_points="""\
+      [paste.app_factory]
+      main = bs12_discovery:main
+      """,
+      )
